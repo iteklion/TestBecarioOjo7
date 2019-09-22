d13a1ad559c50176de9ba458cfb99695c7696512
Itzae Acosta <ItekLion@Itzaes-MacBook-Pro.local>
2019-09-21 21:57:27 -0500

    Se termino la vista del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-index">
+    <input maxlength="5" type="text" v-on:input="ctrlInput($event)" v-model="codigoPostal">
+    <input :disabled=" codigoPostal.length < 5 ? true : false " type="button" value="Buscar" v-on:click="click()">
+    <div v-if="x">
+      <select>
+        <option value="">Uno</option>
+        <option value="">Uno</option>
+      </select>
+      <select>
+        <option value="">dos</option>
+        <option value="">Uno</option>
+        <option value="">Uno</option>
+      </select>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      codigoPostal: '',
+      verSelect: false
+    }
+  },
+  methods: {
+    ctrlInput (event) {
+      event.target.value = event.target.value.replace(/\D/g, '')
+      if (event.target.value.length < 5) {
+        this.verSelect = false
+      }
+    },
+    click () {
+      this.verSelect = true
+    }
   }
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
